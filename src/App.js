a3675168e7078d5247375185d833c11c42f31725
Sanit Kumar <sanitkumar2506@gmail.com>
2023-07-29 11:44:04 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import TodoList from './components/ToDoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
